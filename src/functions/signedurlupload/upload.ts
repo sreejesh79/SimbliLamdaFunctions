85e83000217a813df72e7e377d37a674071c979e
sreejesh <sreejesh@kriyatma.com>
2023-11-22 19:10:23 +0530

    added content type to signed url

--- src/functions/signedurlupload/upload.ts
+++ src/functions/signedurlupload/upload.ts
@@ -1,4 +1,5 @@
 import * as AWS from 'aws-sdk';
+import * as mime from 'mime-types';
 const s3 = new AWS.S3();
 
 const expiry = Number(process.env.SIGNED_URL_EXPIRY);
@@ -7,10 +8,12 @@ const expiry = Number(process.env.SIGNED_URL_EXPIRY);
 export const upload = async (payload) => {
    try {
     const bucketName = process.env.S3_BUCKET_NAME;
+    const mimeType = mime.lookup(payload.key);
     const requestObj: any = {
         Bucket: bucketName,
         Key: payload.key,
-        Expires: expiry
+        Expires: expiry,
+        ContentType: mimeType
      };
      const url = await s3.getSignedUrlPromise("putObject", requestObj);
      return url;
